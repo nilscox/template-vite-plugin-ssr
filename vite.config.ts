534cbfa47127651011231c3acdc880be14a1ef0a
nilscox <nilscox.dev@gmail.com>
2023-02-12 03:10:47 +0100

    setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
-import { UserConfig } from 'vite'
+/// <reference types="vitest" />
 
-const config: UserConfig = {
-  plugins: [react(), ssr()]
-}
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import ssr from 'vite-plugin-ssr/plugin';
 
-export default config
+export default defineConfig({
+  plugins: [react(), ssr()],
+  test: {
+    globals: true,
+    watch: false,
+    environment: 'happy-dom',
+    setupFiles: 'src/vitest.setup.ts',
+  },
+});
